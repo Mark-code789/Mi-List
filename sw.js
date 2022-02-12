0aa4eac10a4730ea3e26f319ba336719506d4107
Mark Etale <66547466+Mark-code789@users.noreply.github.com>
2022-02-12 08:57:42 +0300

    update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ let timer;
 let list = [];
 let notification = false;
 let running = false;
-let version = "1.1.27";
+let version = "1.1.29";
 let appShellFiles = [
 	"./src/images/black logo.png",
 	"./src/images/white logo.png",
