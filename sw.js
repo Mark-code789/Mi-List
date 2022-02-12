5c92ae972a472e419393b271d07fb736a1b8c783
Mark Etale <66547466+Mark-code789@users.noreply.github.com>
2022-02-12 23:09:06 +0300

    update sw.js

--- sw.js
+++ sw.js
@@ -128,7 +128,9 @@ function sendMsg(msg) {
 	self.clients.matchAll({includeUncontrolled: true, type: 'window'}).
 	then((clients) => {
 		if(clients && clients.length) {
-			clients[0].postMessage(msg);
+			for(let client of clients) {
+				client[0].postMessage(msg);
+			} 
 		} 
 	});
 } 
