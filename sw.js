89cce1d287164b2f4fb371604fa23f51fd1b7c52
Mark Etale <66547466+Mark-code789@users.noreply.github.com>
2022-02-13 20:35:09 +0300

    update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ let cacheName = "Mi List";
 let timer;
 let list = [];
 let showNotification = false;
-let version = "1.67.10.72";
+let version = "1.68.10.73";
 let appShellFiles = [
 	"./src/images/black logo.png",
 	"./src/images/white logo.png",
