fdfa8633347dff48dc09e2bff20c84073f5b1b95
Mark Etale <66547466+Mark-code789@users.noreply.github.com>
2022-02-16 20:47:03 +0300

    update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-let version = "53";
+let version = "54";
 let cacheName = "Mi List-v:" + version;
 let timer;
 let list = [];
@@ -103,7 +103,7 @@ self.addEventListener("notificationclick", (e) => {
 	let notification = e.notification;
 	let action = e.action;
 	notification.close();
-	console.log(action);
+	
 	if(action == "check") {
 		let event = notification.data.event;
 		for(let item of list) {
